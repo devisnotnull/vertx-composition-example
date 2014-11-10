2a6bfb28e9ee2ed6f286d56c518f884cfc643d56
David <david@Davids-MacBook.local>
2014-11-10 09:39:33 +0100

    added final integration tests

--- src/main/java/com/mycompany/myproject/MainVerticle.java
+++ src/main/java/com/mycompany/myproject/MainVerticle.java
@@ -13,62 +13,74 @@ import org.vertx.java.platform.Verticle;
 
 public class MainVerticle extends Verticle {
 
-	@Override
-	public void start() {
-		
-		// register handlers
-		vertx.eventBus().registerHandler("sum", new SumHandler());
-		vertx.eventBus().registerHandler("square", new SquareHandler());
-		vertx.eventBus().registerHandler("inc", new IncHandler());
-		vertx.eventBus().registerHandler("range", new RangeHandler());
-		
-		
-		
-		// register web handlers
-		RouteMatcher rm = new RouteMatcher();
-				
-		rm.post("/sum", new RequestBodyHandoffHandler(vertx, "sum"));
-		rm.post("/square", new RequestBodyHandoffHandler(vertx, "square"));
-		rm.post("/inc", new RequestBodyHandoffHandler(vertx, "inc"));
-		rm.post("/range", new RequestBodyHandoffHandler(vertx, "range"));
-		
-		vertx.createHttpServer().requestHandler(rm).listen(8080);
-	}
-	
-	/**
-	 * Get the body of the current (post) request and hand it off to
-	 * some buss-address. register for reply and then pass it back as http response.
-	 * @author Eckart
-	 *
-	 */
-	public static class RequestBodyHandoffHandler implements Handler<HttpServerRequest> {
+    @Override
+    public void start() {
 
-		private static final Logger LOG = Logger.getLogger(RequestBodyHandoffHandler.class.getName());
-		private Vertx vertxInstance;
-		private String targetAddress;
-		
-		public RequestBodyHandoffHandler(Vertx vertx, String address) {
-			this.vertxInstance = vertx;
-			this.targetAddress = address;
-		}
-		
-		@Override
-		public void handle(final HttpServerRequest r) {
-			r.bodyHandler(new Handler<Buffer>(){
-				public void handle(Buffer body) {
-					vertxInstance.eventBus().send(targetAddress, body, new Handler<Message<Buffer>>(){
-						public void handle(Message<Buffer> event) {
-							LOG.log(Level.FINE, "replying with: " + event.body());
-							r.response().end(event.body());								
-						}						
-					});
-					
-				}
-				
-			});
-			
-		}
-		
-	}
-	
+        // register handlers
+        vertx.eventBus().registerHandler("sum", new SumHandler());
+        vertx.eventBus().registerHandler("square", new SquareHandler());
+        vertx.eventBus().registerHandler("inc", new IncHandler());
+        vertx.eventBus().registerHandler("range", new RangeHandler());
+
+        // the naive implementation
+        vertx.eventBus().registerHandler("comp", new CompositionHandler(vertx.eventBus(), new String[]{"square", "inc", "sum"}));
+        vertx.eventBus().registerHandler("comp.configurable", new CompositionHandler(vertx.eventBus()));
+
+        // the rx implementation
+        vertx.eventBus().registerHandler("comp.rx", new RxCompositionHandler(vertx.eventBus(), new String[]{"square", "inc", "sum"}));
+        vertx.eventBus().registerHandler("comp.rx.configurable", new RxCompositionHandler(vertx.eventBus()));
+
+        RouteMatcher rm = new RouteMatcher();
+
+        rm.post("/sum", new RequestBodyHandoffHandler(vertx, "sum"));
+        rm.post("/square", new RequestBodyHandoffHandler(vertx, "square"));
+        rm.post("/inc", new RequestBodyHandoffHandler(vertx, "inc"));
+        rm.post("/range", new RequestBodyHandoffHandler(vertx, "range"));
+        rm.post("/comp", new RequestBodyHandoffHandler(vertx, "comp"));
+        rm.post("/comp.configurable", new RequestBodyHandoffHandler(vertx, "comp.configurable"));
+        rm.post("/comp.rx", new RequestBodyHandoffHandler(vertx, "comp.rx"));
+        rm.post("/comp.rx.configurable", new RequestBodyHandoffHandler(vertx, "comp.rx.configurable"));
+
+        vertx.createHttpServer().requestHandler(rm).listen(8080);
+    }
+
+    /**
+     * Get the body of the current (post) request and hand it off to some buss-address. register for reply and
+     * then pass it back as http response.
+     *
+     * @author Eckart
+     *
+     */
+    public static class RequestBodyHandoffHandler implements Handler<HttpServerRequest> {
+
+        private static final Logger LOG = Logger.getLogger(RequestBodyHandoffHandler.class.getName());
+        private Vertx vertxInstance;
+        private String targetAddress;
+
+        public RequestBodyHandoffHandler(Vertx vertx, String address) {
+            this.vertxInstance = vertx;
+            this.targetAddress = address;
+        }
+
+        @Override
+        public void handle(final HttpServerRequest r) {
+
+            r.bodyHandler(new Handler<Buffer>() {
+                public void handle(Buffer body) {
+                    vertxInstance.eventBus().send(targetAddress, body, new Handler<Message<Buffer>>() {
+                        public void handle(Message<Buffer> event) {
+                            LOG.log(Level.FINE, "replying with: " + event.body());
+                            r.response().end(event.body());
+                        }
+                    });
+//                    }
+
+                }
+
+            });
+
+        }
+
+    }
+
 }
